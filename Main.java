c16cf78c132a4c4d13b785976df7516d2aebc1ba
Askar Kasimov <kasimovaskar123@gmail.com>
2024-11-21 00:25:30 +0300

    model's ready, scenario in progress

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import Creature.Reptile;
+import Exceptions.ReproductionWithItselfException;
+import enums.MasterSkillEnum;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Reptile reptile = new Reptile("mark", MasterSkillEnum.MASTER, 0.5f, 0.5f);
+        try {
+            Reptile newReptile = reptile.makeChildWithPartner(new Reptile("eva", MasterSkillEnum.MASTER, 0.5f, 0.5f));
+            System.out.println(newReptile);
+        } catch (ReproductionWithItselfException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
